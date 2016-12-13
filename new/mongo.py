6a82009ee76bdab38770641813ef30ab2d4feb80
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2016-12-13 08:08:00 +0100

    db client

---
+++ new/mongo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+from pymongo.son_manipulator import SONManipulator
+from species import Species
+from bson.binary import Binary
+
+
+class MongoHandler():
+    def __init__(self, birds):
+        """Establish a connection with the database server
+            and switch to the right database and collection.
+        """
+        self.client = MongoClient()
+        if birds:
+            self.db = self.client.birds
+        else:
+            self.db = self.client.butterflies
+        self.collection = self.db.species
+
+    def addSpecies(self, specie):
+       self.collection.insert(specie)
+
+    def close(self):
+        self.client.close()
+
+
+dbController = MongoHandler(True) 
+
+#spec = Species("Joriss")
+#dbController.addSpecies(spec.toDic())
+
+result =  dbController.collection.find_one({"name":"Joriss"})
+print(result)
+
+dbController.collection.replace_one({"name": "Joriss"},
+                                    {"name":"Sjaak"})
+
+print dbController.collection.find_one()
+
+dbController.close()
